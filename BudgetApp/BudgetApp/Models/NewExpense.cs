30eb861e0b2a49bd3a3b9051fe767da3f9f7a059
angie <75858331+ahalsmer@users.noreply.github.com>
2023-04-04 20:05:44 -0400

    NewExpense class added

---
+++ BudgetApp/BudgetApp/Models/NewExpense.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BudgetApp.Models
+{
+    internal class NewExpense
+    {
+        public string Amount { get; set; }
+        public DateTime Date { get; set; }
+        public string Type { get; set; }
+        public string FileName { get; set; }
+    }
+}
