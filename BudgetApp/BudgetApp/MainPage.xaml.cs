31144167f580798ecd279ddba6a6e5154ec38c31
angie <75858331+ahalsmer@users.noreply.github.com>
2023-04-12 18:57:39 -0400

    implementing a flyout menu (conflicts in ProgressPage)

--- BudgetApp/BudgetApp/MainPage.xaml.cs
+++ BudgetApp/BudgetApp/MainPage.xaml.cs
@@ -12,195 +12,23 @@ using Xamarin.Forms;
 
 namespace BudgetApp
 {
-    public partial class MainPage : ContentPage
+    public partial class MainPage : FlyoutPage
     {
         public MainPage()
         {
             InitializeComponent();
+            flyout.listview.ItemSelected += OnSelectedItem;
         }
-        protected override void OnAppearing()
+
+        private void OnSelectedItem(object sender, SelectedItemChangedEventArgs e)
         {
-            var expenses = new List<NewExpense>();
-
-            // returns an enumerable collection of file names in the specified format
-            var files = Directory.EnumerateFiles(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "*.expense.txt");
-            foreach (var file in files)
+            var item = e.SelectedItem as FlyoutItemPage;
+            if (item != null)
             {
-                var content = File.ReadAllText(file);
-                var details = content.Split('-');
-
-                // for each file in the in the aforementioned list, read the amount, expense, date, and type
-                var expense = new NewExpense()
-                {
-                    Amount = details[0],
-                    Date = File.GetCreationTime(file),
-                    Type = details[1],
-                    Details = $"{File.GetCreationTime(file)}  - {details[1]}",
-                    FileName = file
-                };
-                // add each expense to a list of expenses
-                expenses.Add(expense);
-            }
-            // place expense items in descending order by date
-            ExpenseView.ItemsSource = expenses.OrderByDescending(t => t.Date);
-
-            string FileName = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), $"goals.json");
-            //File.Delete( FileName );
-
-            if (File.Exists(FileName))
-            {
-                string json = File.ReadAllText(FileName);
-                Goal goals = JsonConvert.DeserializeObject<Goal>(json);
-                var balanceList = new List<Balance>();
-                int FoodBalance = goals.Food;
-                int ShoppingBalance = goals.Shopping;
-                int HousingBalance = goals.Housing;
-                int TransportationBalance = goals.Transportation;
-                int EntertainmentBalance = goals.Entertainment;
-                int MiscellaneousBalance = goals.Miscellaneous;
-                foreach (var item in expenses)
-                {
-                    if (item.Amount != "")
-                    {
-                        if (item.Type == "Food")
-                        {
-                            FoodBalance -= int.Parse(item.Amount);
-                        }
-                        if (item.Type == "Shopping")
-                        {
-                            ShoppingBalance -= int.Parse(item.Amount);
-                        }
-                        if (item.Type == "Housing")
-                        {
-                            HousingBalance -= int.Parse(item.Amount);
-                        }
-                        if (item.Type == "Transportation")
-                        {
-                            TransportationBalance -= int.Parse(item.Amount);
-                        }
-                        if (item.Type == "Entertainment")
-                        {
-                            EntertainmentBalance -= int.Parse(item.Amount);
-                        }
-                        if (item.Type == "Miscellaneous")
-                        {
-                            MiscellaneousBalance -= int.Parse(item.Amount);
-                        }
-                    }
-                }
-                var FoodBalanceInfo = new Balance
-                {
-                    Type = "Food",
-                    value = FoodBalance
-                };
-                var ShoppingBalanceInfo = new Balance
-                {
-                    Type = "Shopping",
-                    value = ShoppingBalance
-                };
-                var HousingBalanceInfo = new Balance
-                {
-                    Type = "Housing",
-                    value = HousingBalance
-                };
-                var TransportationBalanceInfo = new Balance
-                {
-                    Type = "Transportation",
-                    value = TransportationBalance
-                };
-                var EntertainmentBalanceInfo = new Balance
-                {
-                    Type = "Entertainment",
-                    value = EntertainmentBalance
-                };
-                var MiscellaneousBalanceInfo = new Balance
-                {
-                    Type = "Miscellaneous",
-                    value = MiscellaneousBalance
-                };
-
-                balanceList.Add(FoodBalanceInfo);
-                balanceList.Add(ShoppingBalanceInfo);
-                balanceList.Add(HousingBalanceInfo);
-                balanceList.Add(TransportationBalanceInfo);
-                balanceList.Add(EntertainmentBalanceInfo);
-                balanceList.Add(MiscellaneousBalanceInfo);
-
-                GoalListView.ItemsSource = balanceList;
-                int total = 0;
-                json = File.ReadAllText(FileName);
-                Goal goalObject = JsonConvert.DeserializeObject<Goal>(json);
-                Debug.WriteLine(File.ReadAllText(FileName));
-                if (goalObject != null)
-                {
-                    total = goalObject.Food + goalObject.Housing + goalObject.Miscellaneous + goalObject.Transportation + goalObject.Shopping + goalObject.Entertainment;
-                }
-                goal.Text = "Total Budget set is " + total;
+                Detail = new NavigationPage((Page)Activator.CreateInstance(item.TargetPage));
+                flyout.listview.SelectedItem = null;
+                IsPresented = false;
             }
         }
-
-        private void ExpenseListView_ItemSelected(object sender, SelectedItemChangedEventArgs e)
-        {
-            Navigation.PushModalAsync(new NewExpensePage
-            {
-                BindingContext = (NewExpense)e.SelectedItem
-            });
-        }
-
-        private void Expense_Clicked(object sender, EventArgs e)
-        {
-            Navigation.PushModalAsync(new NewExpensePage
-            {
-                BindingContext = new NewExpense()
-            });
-        }
-
-        private void Add_Goals(object sender, EventArgs e)
-        {
-            string FileName = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), $"goals.json");
-
-            if (File.Exists(FileName))
-            {
-                string json = File.ReadAllText(FileName);
-                Goal goalObject = JsonConvert.DeserializeObject<Goal>(json);
-
-                Navigation.PushModalAsync(new GoalPage
-                {
-                    BindingContext = goalObject
-                });
-                int total = 0;
-                json = File.ReadAllText(FileName);
-                goalObject = JsonConvert.DeserializeObject<Goal>(json);
-                Debug.WriteLine(File.ReadAllText(FileName));
-                if (goalObject != null)
-                {
-                    total = goalObject.Food + goalObject.Housing + goalObject.Miscellaneous + goalObject.Transportation + goalObject.Shopping + goalObject.Entertainment;
-                }
-                goal.Text = "Total Budget set is " + total;
-
-            }
-            else
-            {
-
-                Navigation.PushModalAsync(new GoalPage
-                {
-                    BindingContext = new Goal()
-                });
-
-                int total = 0;
-                string json = File.ReadAllText(FileName);
-                Goal goalObject = JsonConvert.DeserializeObject<Goal>(json);
-                Debug.WriteLine(File.ReadAllText(FileName));
-                if (goalObject != null)
-                {
-                    total = goalObject.Food + goalObject.Housing + goalObject.Miscellaneous + goalObject.Transportation + goalObject.Shopping + goalObject.Entertainment;
-                }
-                goal.Text = "Total Budget set is " + total;
-
-
-            }
-        }
-
-
     }
 }
---
+++ BudgetApp/BudgetApp/YourExpensesPage.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BudgetApp.Models;
+using Xamarin.Forms;
+
+namespace BudgetApp
+{
+    public partial class YourExpensesPage : ContentPage
+    {
+        public YourExpensesPage()
+        {
+            InitializeComponent();
+        }
+        protected override void OnAppearing()
+        {
+            var expenses = new List<NewExpense>();
+
+            // returns an enumerable collection of file names in the specified format
+            var files = Directory.EnumerateFiles(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "*.notes.txt");
+            foreach (var file in files)
+            {
+                var content = File.ReadAllText(file);
+                var details = content.Split('-');
+
+                // for each file in the in the aforementioned list, read the amount, expense, date, and type
+                var expense = new NewExpense()
+                {
+                    Amount = details[0],
+                    Date = File.GetCreationTime(file),
+                    Type = details[0],
+                    Details = $"{File.GetCreationTime(file)}  - {details.Length}",
+                    FileName = file
+                };
+                // add each expense to a list of expenses
+                expenses.Add(expense);
+            }
+            // place expense items in descending order by date
+            NewExpenseListView.ItemsSource = expenses.OrderByDescending(t => t.Date);
+        }
+
+        private void NewExpenseListView_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            Navigation.PushModalAsync(new NewExpensePage
+            {
+                BindingContext = (NewExpense)e.SelectedItem
+            });
+        }
+
+        private void NewExpense_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushModalAsync(new NewExpensePage
+            {
+                BindingContext = new NewExpense()
+            });
+        }
+    }
+}
