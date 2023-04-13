4f233aa5152936c2a25c65d24a431d7ba3566933
Reshma  Ramachandran <r.reshma67@gmail.com>
2023-04-13 01:22:30 -0700

    adding-flyout

---
+++ BudgetApp/BudgetApp/ExpenseView.xaml.cs
@@ -0,0 +1,62 @@
+﻿using BudgetApp.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace BudgetApp
+{
+    public partial class ExpenseView : ContentPage
+    {
+        public ExpenseView()
+        {
+            InitializeComponent();
+        }
+        protected override void OnAppearing()
+        {
+            var expenses = new List<NewExpense>();
+
+            // returns an enumerable collection of file names in the specified format
+            var files = Directory.EnumerateFiles(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "*.expense.txt");
+            foreach (var file in files)
+            {
+                var content = File.ReadAllText(file);
+                var details = content.Split('-');
+
+                // for each file in the in the aforementioned list, read the amount, expense, date, and type
+                var expense = new NewExpense()
+                {
+                    Amount = details[0],
+                    Date = File.GetCreationTime(file),
+                    Type = details[1],
+                    Details = $"{File.GetCreationTime(file)}  - {details[1]}",
+                    FileName = file
+                };
+                // add each expense to a list of expenses
+                expenses.Add(expense);
+            }
+            // place expense items in descending order by date
+            NewExpenseListView.ItemsSource = expenses.OrderByDescending(t => t.Date);
+        }
+
+        private void NewExpenseListView_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            Navigation.PushModalAsync(new NewExpensePage
+            {
+                BindingContext = (NewExpense)e.SelectedItem
+            });
+        }
+
+        private void NewExpense_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushModalAsync(new NewExpensePage
+            {
+                BindingContext = new NewExpense()
+            });
+        }
+    }
+}
---
+++ BudgetApp/BudgetApp/FlyoutItemPage.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BudgetApp
+{
+    internal class FlyoutItemPage
+    {
+        public String Title { get; set; }
+        public Type TargetPage { get; set; }
+
+    }
+}
